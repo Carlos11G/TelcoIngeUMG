84900db816d385b0990edb62f1a90fdd7b972324
Carlos11G <cjrodasramirez@gmail.com>
2025-07-19 10:51:27 -0600

    Agregar archivos de proyecto.

---
+++ CapaPresentacion/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace CapaPresentacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.iconToolStripButton1 = new FontAwesome.Sharp.IconToolStripButton();
+            this.kryptonSplitContainer1 = new ComponentFactory.Krypton.Toolkit.KryptonSplitContainer();
+            this.kryptonHeaderGroup1 = new ComponentFactory.Krypton.Toolkit.KryptonHeaderGroup();
+            this.kryptonHeaderGroup2 = new ComponentFactory.Krypton.Toolkit.KryptonHeaderGroup();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.iconToolStripButton2 = new FontAwesome.Sharp.IconToolStripButton();
+            this.dgvempleados = new ComponentFactory.Krypton.Toolkit.KryptonDataGridView();
+            this.kryptonTextBox1 = new ComponentFactory.Krypton.Toolkit.KryptonTextBox();
+            this.buttonSpecAny1 = new ComponentFactory.Krypton.Toolkit.ButtonSpecAny();
+            this.kryptonTextBox2 = new ComponentFactory.Krypton.Toolkit.KryptonTextBox();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonSplitContainer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonSplitContainer1.Panel1)).BeginInit();
+            this.kryptonSplitContainer1.Panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonSplitContainer1.Panel2)).BeginInit();
+            this.kryptonSplitContainer1.Panel2.SuspendLayout();
+            this.kryptonSplitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonHeaderGroup1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonHeaderGroup1.Panel)).BeginInit();
+            this.kryptonHeaderGroup1.Panel.SuspendLayout();
+            this.kryptonHeaderGroup1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonHeaderGroup2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonHeaderGroup2.Panel)).BeginInit();
+            this.kryptonHeaderGroup2.Panel.SuspendLayout();
+            this.kryptonHeaderGroup2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvempleados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.iconToolStripButton1,
+            this.toolStripSeparator1,
+            this.iconToolStripButton2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(793, 27);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // iconToolStripButton1
+            // 
+            this.iconToolStripButton1.IconChar = FontAwesome.Sharp.IconChar.UserPlus;
+            this.iconToolStripButton1.IconColor = System.Drawing.Color.Black;
+            this.iconToolStripButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconToolStripButton1.IconSize = 90;
+            this.iconToolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.iconToolStripButton1.Name = "iconToolStripButton1";
+            this.iconToolStripButton1.Size = new System.Drawing.Size(99, 24);
+            this.iconToolStripButton1.Text = "AGREGAR";
+            // 
+            // kryptonSplitContainer1
+            // 
+            this.kryptonSplitContainer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.kryptonSplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.kryptonSplitContainer1.Location = new System.Drawing.Point(0, 27);
+            this.kryptonSplitContainer1.Name = "kryptonSplitContainer1";
+            // 
+            // kryptonSplitContainer1.Panel1
+            // 
+            this.kryptonSplitContainer1.Panel1.Controls.Add(this.kryptonHeaderGroup2);
+            // 
+            // kryptonSplitContainer1.Panel2
+            // 
+            this.kryptonSplitContainer1.Panel2.Controls.Add(this.kryptonHeaderGroup1);
+            this.kryptonSplitContainer1.Size = new System.Drawing.Size(793, 471);
+            this.kryptonSplitContainer1.SplitterDistance = 264;
+            this.kryptonSplitContainer1.TabIndex = 1;
+            // 
+            // kryptonHeaderGroup1
+            // 
+            this.kryptonHeaderGroup1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.kryptonHeaderGroup1.Location = new System.Drawing.Point(0, 0);
+            this.kryptonHeaderGroup1.Name = "kryptonHeaderGroup1";
+            // 
+            // kryptonHeaderGroup1.Panel
+            // 
+            this.kryptonHeaderGroup1.Panel.Controls.Add(this.dgvempleados);
+            this.kryptonHeaderGroup1.Size = new System.Drawing.Size(524, 471);
+            this.kryptonHeaderGroup1.StateNormal.HeaderPrimary.Content.LongText.TextH = ComponentFactory.Krypton.Toolkit.PaletteRelativeAlign.Center;
+            this.kryptonHeaderGroup1.StateNormal.HeaderPrimary.Content.LongText.TextV = ComponentFactory.Krypton.Toolkit.PaletteRelativeAlign.Center;
+            this.kryptonHeaderGroup1.TabIndex = 0;
+            this.kryptonHeaderGroup1.ValuesPrimary.Description = "Listado de Empleados";
+            this.kryptonHeaderGroup1.ValuesPrimary.Heading = "";
+            this.kryptonHeaderGroup1.ValuesPrimary.Image = null;
+            this.kryptonHeaderGroup1.ValuesSecondary.Description = "Registros: 0";
+            this.kryptonHeaderGroup1.ValuesSecondary.Heading = "";
+            // 
+            // kryptonHeaderGroup2
+            // 
+            this.kryptonHeaderGroup2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.kryptonHeaderGroup2.HeaderVisibleSecondary = false;
+            this.kryptonHeaderGroup2.Location = new System.Drawing.Point(0, 0);
+            this.kryptonHeaderGroup2.Name = "kryptonHeaderGroup2";
+            // 
+            // kryptonHeaderGroup2.Panel
+            // 
+            this.kryptonHeaderGroup2.Panel.Controls.Add(this.kryptonTextBox2);
+            this.kryptonHeaderGroup2.Panel.Controls.Add(this.kryptonTextBox1);
+            this.kryptonHeaderGroup2.Size = new System.Drawing.Size(264, 471);
+            this.kryptonHeaderGroup2.StateNormal.HeaderPrimary.Content.LongText.TextH = ComponentFactory.Krypton.Toolkit.PaletteRelativeAlign.Center;
+            this.kryptonHeaderGroup2.StateNormal.HeaderPrimary.Content.LongText.TextV = ComponentFactory.Krypton.Toolkit.PaletteRelativeAlign.Center;
+            this.kryptonHeaderGroup2.TabIndex = 0;
+            this.kryptonHeaderGroup2.ValuesPrimary.Description = "Captura de Datos";
+            this.kryptonHeaderGroup2.ValuesPrimary.Heading = "";
+            this.kryptonHeaderGroup2.ValuesPrimary.Image = null;
+            this.kryptonHeaderGroup2.ValuesSecondary.Heading = "";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 27);
+            // 
+            // iconToolStripButton2
+            // 
+            this.iconToolStripButton2.IconChar = FontAwesome.Sharp.IconChar.Users;
+            this.iconToolStripButton2.IconColor = System.Drawing.Color.Black;
+            this.iconToolStripButton2.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconToolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.iconToolStripButton2.Name = "iconToolStripButton2";
+            this.iconToolStripButton2.Size = new System.Drawing.Size(72, 24);
+            this.iconToolStripButton2.Text = "Editar";
+            // 
+            // dgvempleados
+            // 
+            this.dgvempleados.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgvempleados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvempleados.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvempleados.Location = new System.Drawing.Point(0, 0);
+            this.dgvempleados.Name = "dgvempleados";
+            this.dgvempleados.ReadOnly = true;
+            this.dgvempleados.RowHeadersWidth = 51;
+            this.dgvempleados.RowTemplate.Height = 24;
+            this.dgvempleados.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvempleados.Size = new System.Drawing.Size(522, 415);
+            this.dgvempleados.TabIndex = 0;
+            this.dgvempleados.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.kryptonDataGridView1_CellContentClick);
+            // 
+            // kryptonTextBox1
+            // 
+            this.kryptonTextBox1.ButtonSpecs.AddRange(new ComponentFactory.Krypton.Toolkit.ButtonSpecAny[] {
+            this.buttonSpecAny1});
+            this.kryptonTextBox1.Location = new System.Drawing.Point(58, 54);
+            this.kryptonTextBox1.Name = "kryptonTextBox1";
+            this.kryptonTextBox1.Size = new System.Drawing.Size(164, 27);
+            this.kryptonTextBox1.TabIndex = 0;
+            // 
+            // buttonSpecAny1
+            // 
+            this.buttonSpecAny1.UniqueName = "8D65777994F3469EBF8E14AF16C84048";
+            // 
+            // kryptonTextBox2
+            // 
+            this.kryptonTextBox2.Location = new System.Drawing.Point(58, 151);
+            this.kryptonTextBox2.Name = "kryptonTextBox2";
+            this.kryptonTextBox2.Size = new System.Drawing.Size(171, 27);
+            this.kryptonTextBox2.TabIndex = 1;
+            this.kryptonTextBox2.Text = "kryptonTextBox2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(793, 498);
+            this.Controls.Add(this.kryptonSplitContainer1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonSplitContainer1.Panel1)).EndInit();
+            this.kryptonSplitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonSplitContainer1.Panel2)).EndInit();
+            this.kryptonSplitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonSplitContainer1)).EndInit();
+            this.kryptonSplitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonHeaderGroup1.Panel)).EndInit();
+            this.kryptonHeaderGroup1.Panel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonHeaderGroup1)).EndInit();
+            this.kryptonHeaderGroup1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonHeaderGroup2.Panel)).EndInit();
+            this.kryptonHeaderGroup2.Panel.ResumeLayout(false);
+            this.kryptonHeaderGroup2.Panel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonHeaderGroup2)).EndInit();
+            this.kryptonHeaderGroup2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvempleados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private FontAwesome.Sharp.IconToolStripButton iconToolStripButton1;
+        private ComponentFactory.Krypton.Toolkit.KryptonSplitContainer kryptonSplitContainer1;
+        private ComponentFactory.Krypton.Toolkit.KryptonHeaderGroup kryptonHeaderGroup1;
+        private ComponentFactory.Krypton.Toolkit.KryptonHeaderGroup kryptonHeaderGroup2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private FontAwesome.Sharp.IconToolStripButton iconToolStripButton2;
+        private ComponentFactory.Krypton.Toolkit.KryptonDataGridView dgvempleados;
+        private ComponentFactory.Krypton.Toolkit.KryptonTextBox kryptonTextBox1;
+        private ComponentFactory.Krypton.Toolkit.ButtonSpecAny buttonSpecAny1;
+        private ComponentFactory.Krypton.Toolkit.KryptonTextBox kryptonTextBox2;
+    }
+}
+
